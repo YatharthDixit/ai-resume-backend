29d10e9d9410e52b0d6abf47635275153b4850b3
Yatharth Dixit <yatharthdixit@outlook.com>
2025-12-11 19:07:22 +0530

    Revert "feat: configure Pino logger with pino-pretty for enhanced log formatting."
    This reverts commit f1d21ddd9da7d7c7ef405cd65e68cb2dde7f5e09.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,14 +2,8 @@ const pino = require('pino');
 
 const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-    },
-  },
+  // In development, pino-colada will prettify this
+  // In production, it will be standard JSON
 });
 
 module.exports = logger;
